50608e5a4458273dcf9d0cf6db16acebdf1de925
Juan Albahaca <juanalbahaca92@gmail.com>
2018-01-05 22:11:12 +0100

    first commit

---
+++ Mandelbrot/Mandelbrot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mandelbrot
+//
+//  Created by Juan Carlos Albahaca on 2018-01-05.
+//  Copyright © 2018 Juan Carlos Albahaca. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
